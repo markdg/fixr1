bba8fd63c07596aca963311aa5d11f146d572d9f
Mark <mark@markgeorge.net>
2015-04-05 19:45:22 +0700

    Updated dummy config file

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
-# Replace items in brackets with your own
-HOST = '<your-ftp-server>'
-USERNAME = '<your-username>'
-PASSWORD = '<your-password>'
-BASEDIR = '<your-starting-directory>'
-KEYWORD = '<keyword-for-delete-line>'
-BADWORD = '<badword-for-flag-file>'
-FILENAME = '<file-name-for-delete-file>'
+# Replace items in quotes
+HOST = 'ftp-server'
+USERNAME = 'username'
+PASSWORD = 'password'
+BASEDIR = 'starting-directory'
+KEYWORD = 'keyword-for-delete-line'
+FILENAME = 'file-name-for-delete-file'
